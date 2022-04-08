fa271f0c5dccbefc3e85fd24087fa894c420cdb4
Dzaki Wiradinata <dzaki.wiradinata@gmail.com>
2022-04-08 17:52:12 -0400

    Created functions of addition, subtraction, multiplication, and division

---
+++ calc.js
@@ -0,0 +1,30 @@
+function add(x,y) {
+    return x + y;
+}
+
+function subtract(x,y) {
+    return x - y;
+}
+
+function multiply(x,y) {
+    return x * y;
+}
+
+function divide(x,y) {
+    return x / y;
+}
+
+function operate(x,op,y) {
+    if(op === '+') {
+        return add(x,y);
+    }
+    else if(op === '-') {
+        return subtract(x,y);
+    }
+    else if(op === '*') {
+        return multiply(x,y);
+    } 
+    else if(op === '/'){
+        return divide(x,y);
+    }
+}
